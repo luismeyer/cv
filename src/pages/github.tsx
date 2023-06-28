2fc3d1c8d97578d4f066b5d29b17e1c759ac98d3
Luis Meyer <luis_meyer@outlook.de>
2023-06-28 13:13:00 +0200

    fix: increase contrast of links

--- src/pages/github.tsx
+++ src/pages/github.tsx
@@ -45,7 +45,7 @@ export function Github(props: PageProps) {
         href="https://github.com/luismeyer"
         class="flex items-center gap-2 md:gap-4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
       >
-        <div class="w-12 h-12 md:w-32 md:h-32">
+        <div class="w-12 h-12 md:w-32 md:h-32 fill-white">
           <GithubLogo />
         </div>
 
